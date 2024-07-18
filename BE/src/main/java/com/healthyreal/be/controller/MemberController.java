d646c74785ee3bc3f7ef7d7060a18f0cf66e7e86
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-19 00:38:40 +0900

    feat: 회원 온보딩 프로토타입 작성 #13

--- BE/src/main/java/com/healthyreal/be/controller/MemberController.java
+++ BE/src/main/java/com/healthyreal/be/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.healthyreal.be.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/members")
 public class MemberController {
 
 	@GetMapping
