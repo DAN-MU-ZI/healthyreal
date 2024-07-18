84df4ae8a76a1c6d91701d53ad309319275d7bcc
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-19 00:37:38 +0900

    feat: cors 보안 적용 및 일부 보안 변경 #7

--- BE/src/main/java/com/healthyreal/be/config/SecurityConfig.java
+++ BE/src/main/java/com/healthyreal/be/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.healthyreal.be.config;
 import com.healthyreal.be.service.CustomOAuth2UserService;
 import com.healthyreal.be.service.JwtAuthenticationFilter;
 import com.healthyreal.be.service.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+	private final CorsProperties corsProperties;
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository;
 	private final CustomOAuth2UserService oAuth2UserService;
@@ -28,7 +33,9 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(csrf -> csrf
-				.ignoringRequestMatchers(PathRequest.toH2Console()))
+				.ignoringRequestMatchers(PathRequest.toH2Console()).disable())
+			.cors(cors -> cors
+				.configurationSource(corsConfigurationSource()))
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.logout(AbstractHttpConfigurer::disable)
@@ -43,6 +50,7 @@ public class SecurityConfig {
 				.requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
 				.requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
 				.requestMatchers("/login", "/oauth2/**").permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/api/onboarding/**")).permitAll()
 				.anyRequest().authenticated()
 			)
 
@@ -58,5 +66,19 @@ public class SecurityConfig {
 			.addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
+		corsConfig.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
+		corsConfig.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setMaxAge(corsConfig.getMaxAge());
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
 	}
 }
