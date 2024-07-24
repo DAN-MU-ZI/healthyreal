998e9f162ead79c464fde1a090a8fb3ed908ec26
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-24 20:19:20 +0900

    refactor: 클라우드 요청으로 변경

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -15,11 +15,7 @@ const UsersPage: React.FC = () => {
       try {
         const token = localStorage.getItem('token');
         if (token) {
-          // const response = await axios.get<User[]>('http://localhost:8080/api/v1/users', {
-          //   headers: {
-          //     Authorization: `Bearer ${token}`,
-          //   },
-          // });
+          // const response = await axios.get<User[]>('http://localhost:8080/api/v1/users');
           const response = await axios.get<User[]>('http://default-loadbalancer-ser-e983d-25608891-dc411cb92a75.kr.lb.naverncp.com:8080/api/v1/users');
 
           setResponseData(response.data);
