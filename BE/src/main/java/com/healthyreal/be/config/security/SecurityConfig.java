9d63324f3416ad0e56f51f513c5c4c03d51596b4
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-25 10:38:58 +0900

    feat: frameoption 허용(h2 콘솔목적)

--- BE/src/main/java/com/healthyreal/be/config/security/SecurityConfig.java
+++ BE/src/main/java/com/healthyreal/be/config/security/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -73,6 +74,7 @@ public class SecurityConfig {
 				AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
+			.headers(head -> head.frameOptions(FrameOptionsConfig::sameOrigin))
 			.exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
 				.authenticationEntryPoint(new RestAuthenticationEntryPoint())
 				.accessDeniedHandler(tokenAccessDeniedHandler))
