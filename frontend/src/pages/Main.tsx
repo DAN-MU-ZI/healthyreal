94fbc280fc62fed3d47c8b9a149d2c6f564dda43
ChoiJuyoung <cjy020718@gmail.com>
2024-07-30 20:01:51 +0900

    feat: alarm 세션 구조

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,6 +1,48 @@
 import * as React from "react";
-import PlaceCard from "../components/atoms/PlaceCard";
+import AlarmLayout from "../components/templates/AlarmLayout";
+import AlarmCard from "../components/molecules/AlarmCard";
+import Text from "../components/atoms/Text";
 
 export default function Main() {
-  return <div>Main page</div>;
+  return (
+    <>
+      <header>
+        <Text color="var(--main-blue)" fontSize="30px" fontWeight="700">
+          서비스명
+        </Text>
+      </header>
+      <body>
+        <AlarmLayout
+          title="알림"
+          more="전체 보기"
+          contents={
+            <>
+              <AlarmCard
+                time="10:30"
+                state="에정"
+                title="개인 운동"
+                detail="등, 이두 루틴하는 날"
+                more="자세히 보기"
+              />
+              <AlarmCard
+                time="10:30"
+                state="에정"
+                title="개인 운동"
+                detail="등, 이두 루틴하는 날"
+                more="자세히 보기"
+              />
+              <AlarmCard
+                time="10:30"
+                state="에정"
+                title="개인 운동"
+                detail="등, 이두 루틴하는 날"
+                more="자세히 보기"
+              />
+            </>
+          }
+          onClick={() => console.log("hos")}
+        />
+      </body>
+    </>
+  );
 }
