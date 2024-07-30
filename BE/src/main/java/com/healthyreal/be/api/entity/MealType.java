dd40104ce715e9bbef11551e3c86448a75d0160a
JongHan Jeon <moohan211@naver.com>
2024-07-30 18:28:55 +0900

    feat: 엔티티 추가

---
+++ BE/src/main/java/com/healthyreal/be/api/entity/MealType.java
@@ -0,0 +1,5 @@
+package com.healthyreal.be.api.entity;
+
+public enum MealType {
+	BREAKFAST, LUNCH, DINNER
+}
---
+++ BE/src/main/java/com/healthyreal/be/api/entity/schedule/Routine.java
@@ -0,0 +1,37 @@
+package com.healthyreal.be.api.entity.schedule;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Routine {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "routine_id", nullable = false)
+	private Long id;
+
+	@Column(nullable = false)
+	private Long weight;
+
+	@Column(nullable = false)
+	private Long repetition;
+
+	@ManyToOne
+	@JoinColumn(name = "schedule_id", nullable = false)
+	private Schedule schedule;
+
+}
