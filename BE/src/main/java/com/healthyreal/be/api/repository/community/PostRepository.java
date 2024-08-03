4e06b59ad912f084c0144ce6c062e21043f40b70
dohyoi <gnoeyh19@gmail.com>
2024-08-03 19:51:33 +0900

    refactor: 커뮤니티 페이지 구현 에러 수정
    정상 실행 / 이미지 추가 필요

--- BE/src/main/java/com/healthyreal/be/api/repository/community/PostRepository.java
+++ BE/src/main/java/com/healthyreal/be/api/repository/community/PostRepository.java
@@ -1,4 +1,4 @@
-package com.healthyreal.be.api.repository;
+package com.healthyreal.be.api.repository.community;
 
 import com.healthyreal.be.api.entity.community.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
